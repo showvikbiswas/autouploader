a94f2403bb877d7bdab319aa1cc69a5d4ff7133f
showvikbiswas <64810103+showvikbiswas@users.noreply.github.com>
2021-03-31 01:11:33 +0600

    Update autouploader_mocks_2021.py
    Users do not need to manually delete the token.json anymore.
    Added a few more improvements.

--- autouploader_mocks_2021.py
+++ autouploader_mocks_2021.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 import os.path
+from os import path
 from os import listdir
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -13,6 +14,10 @@ import atexit
 SCOPES = ['https://www.googleapis.com/auth/drive']
 
 def main():
+
+    if os.path.exists('token.json'):
+        os.remove('token.json')
+        
     """Shows basic usage of the Drive v3 API.
     Prints the names and ids of the first 10 files the user has access to.
     """
@@ -55,38 +60,105 @@ def main():
         print('Parent folder set to ' + parent_folder['name'])
         break
 
+    queue = list()
+
     for f in files:
-        tokens = f.split(maxsplit=3)
         print('Trying to upload ' + f)
         # Debug tool
         # if m.getch().decode('ASCII') == 'N':
         #     break
+
+        # Make an initial full match query
+        full_name = f.split(sep='-')[0].rstrip()
+        query = ("name = '" + full_name + "' and " + "mimeType = 'application/vnd.google-apps.folder' and '" + 
+                parent_id + "' in parents and trashed = False")
+        folders = search(query, service)
         
-        query = ("name contains '" + tokens[0] + "' and name contains '" + tokens[1] + "' and "
+        if len(folders) == 1:
+            print('Found perfect match with file name. Uploading to ' + service.files().get(fileId=folders[0].get('id')).execute()['name'] + '...')
+            upload_root = folders[0]
+        
+        else:
+            tokens = full_name.split()
+            if len(tokens) > 1:
+                query = ("name contains '" + tokens[0] + "' and name contains '" + tokens[1] + "' and "
+                    + "mimeType = 'application/vnd.google-apps.folder' and '" + parent_id + "' in parents and trashed = False")
+            else:
+                query = ("name contains '" + tokens[0] + "' and "+ "mimeType = 'application/vnd.google-apps.folder' and '" + parent_id + 
+                        "' in parents and trashed = False")
+                
+            folders = search(query, service)
+
+            if len(folders) == 0:
+                print('Could not find a match for ' + f + '. Adding to queue and moving on...\n')
+                queue.append(f)
+                continue
+            
+            elif len(folders) == 1:
+                print('Found one partial match for ' + f + '. Adding to queue and moving on...\n')
+                queue.append(f)
+                continue
+        
+            else:
+                print('Found multiple matches for ' + f + '. Adding to queue and moving on...\n')
+                queue.append(f)
+                continue
+
+        upload(upload_root, f, parent_folder, query, service)
+        print()
+    
+    if len(queue) > 0:
+        print('In queue...')
+
+    for item in queue:
+        full_name = item.split(sep='-')[0].rstrip()
+        tokens = full_name.split()
+        
+        if len(tokens) > 1:
+            query = ("name contains '" + tokens[0] + "' and name contains '" + tokens[1] + "' and "
                 + "mimeType = 'application/vnd.google-apps.folder' and '" + parent_id + "' in parents and trashed = False")
+        else:
+            query = ("name contains '" + tokens[0] + "' and "+ "mimeType = 'application/vnd.google-apps.folder' and '" + parent_id + 
+                    "' in parents and trashed = False")
+        
         folders = search(query, service)
 
         if len(folders) == 0:
-            print('Could not find a match for ' + f)
-            upload_root = create_new_student_folder(parent_id, f, service)
+            print('There are no matches for ' + item)
+            print('Do you still want to upload the file? <press y to confirm, any other key to cancel>')
+            if m.getch().decode('ASCII') != 'y':
+                print('Upload of ' + item + ' was cancelled by user.')
+                continue
+            upload_root = create_new_student_folder(parent_id, item, service)
         
         elif len(folders) == 1:
-            while True:
-                print('Found folder with name: ' + service.files().get(fileId=folders[0].get('id')).execute()['name'] + '. Do you want to upload to this folder? <y/n>')
-                pressedKey = m.getch().decode('ASCII')
-                if pressedKey == 'y':
-                    upload_root = folders[0]
-                    break
-                else:
-                    upload_root = create_new_student_folder(parent_id, f, service)
-                    break
+            print('There is one partial match for ' + item + ' with name ' +
+                service.files().get(fileId=folders[0].get('id')).execute()['name'])
+            print('Do you still want to upload the file? <press y to confirm, any other key to cancel>')
+            if m.getch().decode('ASCII') != 'y':
+                print('Upload of ' + item + ' was cancelled by user.')
+                continue
+            print('Do you want to upload to ' + service.files().get(fileId=folders[0].get('id')).execute()['name'] + '? <press y to confirm, any other key to upload to a new folder>')
+            pressedKey = m.getch().decode('ASCII')
+            if pressedKey == 'y':
+                upload_root = folders[0]
+            else:
+                upload_root = create_new_student_folder(parent_id, f, service)
         
         else:
-            print('There are ' + str(len(folders)) + ' matches for the current name in the present root. Please choose ID of the folder' + 
-                ' to upload to. Please type -1 and press Enter if you wish to create a new folder.')
+            print('There are ' + str(len(folders)) + ' matches for the current name in the present root.')
+            
             for i in range(len(folders)):
                 possibility = service.files().get(fileId=folders[i].get('id')).execute()
                 print(str(i+1) + ': ' + possibility['name'])
+            
+            print('Do you still want to upload the file? <press y to confirm, any other key to cancel>')
+            if m.getch().decode('ASCII') != 'y':
+                print('Upload of ' + item + ' was cancelled by user.')
+                continue
+            
+            print('Please choose ID of the folder to upload to. Please type -1 and press Enter if you wish to create a new folder.')
+            
             while True:
                 try: 
                     choice = int(input('Choice: '))
@@ -100,46 +172,49 @@ def main():
                         print('Choice is not within range. Please try again.')                                    
                 except:
                     print('Not a number. Try again.')
-            
 
-        upload_root_id = upload_root.get('id')
-        query = "name contains 'MOCKS' and '" + upload_root_id + "' in parents and trashed = False"
-        mock_folder_list = search(query, service)
-        try:
-            upload_folder = create_mock_folder(mock_folder_list, service, upload_root_id)
-        except Exception:
-            error()
-            continue
-
-        upload_folder_id = upload_folder.get('id')
-
-        # Search for preexisting files with given name and delete
-        query = "name = '" + f + "' and '" + upload_folder_id +"' in parents and trashed = False"
-        file_duplicates = search(query, service)
-        if len(file_duplicates) > 0:
-            print('Duplicates found for ' + f + '. Deleting duplicates...')
-            for file in file_duplicates:
-                service.files().delete(fileId=file.get('id')).execute()
-
-        file_metadata = {
-            'name': f,
-            'parents': [upload_folder_id]
-        }
-        try:                
-            media = MediaFileUpload('Scripts\\' + f,
-                                    mimetype='application/pdf',
-                                    resumable=True)
-            print('Uploading...')
-            uploadfile = service.files().create(body=file_metadata,
-                                            media_body=media,
-                                            fields='id').execute()
-            uploadfile = service.files().get(fileId=uploadfile.get('id')).execute() 
-            upload_folder = service.files().get(fileId=upload_root_id).execute()
-            print('Successfully uploaded ' + uploadfile['name'] + ' to ' + parent_folder['name'] + '/' + upload_folder['name'])
-        except:
-            print('An error occurred. File ' + f + ' could not be uploaded.')
-        
+        upload(upload_root, item, parent_folder, query, service)
         print()
+            
+        
+def upload(upload_root, file_name, parent_folder, query, service):
+    upload_root_id = upload_root.get('id')
+    query = "name = 'MOCKS' and '" + upload_root_id + "' in parents and trashed = False"
+    mock_folder_list = search(query, service)
+    try:
+        upload_folder = create_mock_folder(mock_folder_list, service, upload_root_id)
+    except Exception:
+        error()
+        return
+
+    upload_folder_id = upload_folder.get('id')
+    # Search for preexisting files with given name and delete
+    query = "name = '" + file_name + "' and '" + upload_folder_id +"' in parents and trashed = False"
+    file_duplicates = search(query, service)
+    if len(file_duplicates) > 0:
+        print('Duplicates found for ' + file_name + '. Deleting duplicates...')
+        for file in file_duplicates:
+            service.files().delete(fileId=file.get('id')).execute()
+
+    file_metadata = {
+        'name': file_name,
+        'parents': [upload_folder_id]
+    }
+    try:                
+        media = MediaFileUpload('Scripts\\' + file_name,
+                                mimetype='application/pdf',
+                                resumable=True)
+        print('Uploading...')
+        uploadfile = service.files().create(body=file_metadata,
+                                        media_body=media,
+                                        fields='id').execute()
+        uploadfile = service.files().get(fileId=uploadfile.get('id')).execute() 
+        upload_folder = service.files().get(fileId=upload_root_id).execute()
+        print('Successfully uploaded ' + uploadfile['name'] + ' to ' + parent_folder['name'] + '/' + upload_folder['name'])
+    except:
+        print('An error occurred. File ' + file_name + ' could not be uploaded.')
+    
+            
 
 def search(query, service):
     page_token = None
